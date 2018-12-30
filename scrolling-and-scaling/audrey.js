8d07252a65f93c90cbd85ab3820716fdaa21411b
Colleen Woolsey <rlwool@aol.com>
2018-12-29 23:33:29 -0600

    Scrolls and values increment properly, but haven't found command to scale element

--- scrolling-and-scaling/audrey.js
+++ scrolling-and-scaling/audrey.js
@@ -1,14 +1,36 @@
 const audrey = document.getElementById("audrey")
 const container = document.getElementById("container")
+console.log(audrey.innerHTML);
+console.log(container.innerHTML);
+
 /*
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
-container.addEventListener("scroll", function () {
-    document.getElementById("audrey").style.transform = "scale(3, 4)";
-    //document.querySelector(".trans").toggleClass("trans")
-   })   
-    /*
+window.addEventListener("scroll", function () {
+    let ywindow = window.scrollY;
+    console.log("y = " + ywindow);
+    let y3 = ywindow / 3;
+    console.log("y3 = " + y3);
+    let y4 = ywindow / 4;
+    console.log("y4 = " + y4);
+
+    // let cvarheight = container.style.height;
+    // cvarheight = (Math.round(y4) + "px");
+    // let cvarwidth = container.style.width;
+    // cvarwidth = (Math.round(y3) + "px");
+    // console.log("container height = " + cvarheight);
+    // console.log("container width = " + cvarwidth);
+    
+    let avarheight = audrey.style.height;
+    avarheight = (Math.round(y4) + "px");
+    let avarwidth = audrey.style.width;
+    avarwidth = (Math.round(y3) + "px");
+    console.log("audrey height = " + avarheight);
+    console.log("audrey width = " + avarwidth);
+    })
+
+        /*
         Adjust the height of audrey to be 1/4 the value of
         `window.scrollY`. No lower than 100px, though.
     */
